2c79cb2e5077fb900d8b88178b00ee40d6b9f7c0
crispy-cat <the@crispy.cat>
2022-06-05 13:15:06 -0400

    v1.0.3

--- installation_scripts/install.php
+++ installation_scripts/install.php
@@ -1,11 +1,11 @@
 <?php
 	// Copy files
 	installer_message("Copying files");
-	FileHelper::copyRecurs(TMPEXT . "/plugins", Config::APPROOT . "/plugins");
+	FileHelper::copyRecurs(PACKAGE_DIR . "/plugins", \Config::APPROOT . "/plugins");
 	
 	// Register extensions
 	installer_message("Registering extensions");
 	ExtensionHelper::registerExtension("crispycat.tagmanagerplugin/TagManagerPlugin", "plugin", "frontend");
 	
-	installer_message("Tag manager plugin installed!", IMSG_INFO);
+	installer_message("Tag manager plugin installed!", \Crispage\Application\InstallerApplication::IMSG_INFO);
 ?>
--- plugins/crispycat.tagmanagerplugin/TagManagerPlugin.php
+++ plugins/crispycat.tagmanagerplugin/TagManagerPlugin.php
@@ -1,13 +1,15 @@
 <?php
+	namespace Crispage\Plugins;
+
 	defined("CRISPAGE") or die("Application must be started from index.php!");
 
-	class TagManagerPlugin extends Plugin {
+	class TagManagerPlugin extends \Crispage\Assets\Plugin {
 		public function execute() {
 			global $app;
 
 			$app->vars["tm_code"] = $this->options["code"];
 
-			$app->events->registerAction(new EventAction(array(
+			$app->events->registerAction(new \Crispage\Events\EventAction(array(
 				"id" => "crispycat.tagmanagerplugin.render_tm",
 				"event" => "page.pre_render.scripts",
 				"priority" => 0,
@@ -17,3 +19,4 @@
 			)));
 		}
 	}
+?>
